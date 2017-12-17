3260fb30f8ed78f37075c304fa3b8491654dac3e
Marcin Polski <marcin.polski98@gmail.com>
2017-12-17 22:15:50 +0100

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
@@ -34,7 +35,7 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
   const id = req.params.id;
   if (!ObjectID.isValid(id)) {
-    res.status(404).send();
+    return res.status(404).send();
   }
   Todo.findById(id).then((todo) => {
     if (!todo) {
@@ -43,6 +44,47 @@ app.get('/todos/:id', (req, res) => {
     res.send({todo});
   }).catch(() => res.status(400).send());
 })
+
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch(() => res.status(400).send());
+})
+
+app.patch('/todos/:id', (req,res) => {
+  const id = req.params.id;
+  const body = _.pick(req.body, ['text', 'completed']);
+  
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send()
+  });
+});
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
